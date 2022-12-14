32db6a22f7e0bad1d6abdadd83e38883c4c684f0
Taewan Kim <44514409+yuhatown@users.noreply.github.com>
2022-12-15 08:50:01 +0900

    add gin-gonic

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"result": "ok",
+		})
+	})
+	r.Run()
+}
